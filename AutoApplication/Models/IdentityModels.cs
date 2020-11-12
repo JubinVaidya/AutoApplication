fd755dcdeff3c933f15cfe2aac3ba5d7da616dd1
JubinVaidya <jubin.vaidya4@gmail.com>
2020-11-12 12:50:44 -0600

    Changed the default connection string so that actual connection to project database is used.

--- AutoApplication/Models/IdentityModels.cs
+++ AutoApplication/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Configuration;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -21,7 +22,7 @@ namespace AutoApplication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base(ConfigurationManager.ConnectionStrings["AutoDealerVaidyaDB"].ConnectionString, throwIfV1Schema: false)
         {
         }
 
